03a00ae0b059ed1d36296a7dfc072c2daa5aef7b
Tendry <tendryzephyrin@gmail.com>
2024-08-14 01:04:38 +0200

    Petite refactoring

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -4,10 +4,8 @@ namespace App\Controller;
 
 use App\Repository\MedicamentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
-
 
 class APIController extends AbstractController
 {
--- src/Controller/Users/UsersController.php
+++ src/Controller/Users/UsersController.php
@@ -10,7 +10,6 @@ use App\Entity\Vente;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
